8283aa5c09607deb87586a21c259e975f216f789
hanan467 <hananansaresmael@gmail.com>
2024-03-20 16:24:14 +0300

    theatre model

---
+++ app/Models/Theatre.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Theatre extends Model
+{
+    use HasFactory;
+    public function seats(){
+        return $this->hasMany(Seat::class);
+    }
+    public function showtimes(){
+        return $this->hasMany(Showtime::class);
+    }
+}
